0fcbe475e1b7bb3037b42a13020244fd715cdee4
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-18 23:17:40 +0900

    多重ループ２

--- fizbuzz/fizzbuzz6.java
+++ fizbuzz/fizzbuzz6.java
@@ -3,7 +3,7 @@ package fizbuzz;
  class fizzbuzz6 {
   public static void main(String[]ages){
     for(int i=1; i<=10; i++){
-      for(int h=1; h<8; h++){
+      for(int h=1; h<10; h++){
         System.out.print("|"+i*h);
       }
       System.out.println("|");
