1e9394ee7a251660b6648f046a0e23e610402b25
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-18 21:55:39 +0900

    ４の倍数を表示2

---
+++ fizbuzz/fizzbuzz4.java
@@ -0,0 +1,11 @@
+package fizbuzz;
+
+class fizzbuzz4 {
+  public static void main(String[]ages){
+    for(int i=0; i<=36; i++){
+      if(i%4==0){
+        System.out.println(i);
+      }
+    }
+  }  
+}
