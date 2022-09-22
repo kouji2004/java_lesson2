673a3f97e95502d39c82741d34ca51aa39b067e7
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-23 00:41:07 +0900

    総数の練習

---
+++ fizbuzz/lesson/lesson6.java
@@ -0,0 +1,11 @@
+package fizbuzz.lesson;
+
+public class lesson6 {
+  public static void main(String[]ages){
+    int all = 0;
+    for(int i=0; i<=30; i++ ){
+      all = all+i;
+    }
+    System.out.println("総数は"+all);
+  }
+}
