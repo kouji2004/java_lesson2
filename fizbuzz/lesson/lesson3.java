623d5146f086c23832fac7770db95feebff0a53c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-22 18:28:45 +0900

    総和

---
+++ fizbuzz/lesson/lesson3.java
@@ -0,0 +1,12 @@
+package fizbuzz.lesson;
+
+public class lesson3 {
+  public static void main(String[] ages) {
+    int totl = 0;
+    for (int i = 0; i <= 100; i++) {
+      totl = totl + i;
+
+    }
+    System.out.println("1～1000の総和は" + totl);
+  }
+}
